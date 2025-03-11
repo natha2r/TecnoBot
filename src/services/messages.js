6e72cd78759caf672ddb924ad71c11eb8e517a49
Nathalia <karolgalvis10@gmail.com>
2025-03-11 12:00:12 -0500

    manejo de variables para mensajes

---
+++ src/services/messages.js
@@ -0,0 +1,88 @@
+// Mensajes de Bienvenida y Menú Principal
+export const WELCOME_MESSAGE = (name) => ` 👋 ¡Hola ${name}! Soy el asistente virtual del Tecnoparque Nodo Bucaramanga Bienvenido. 🚀\n\n¿En qué puedo ayudarte?`;
+
+export const MAIN_MENU_MESSAGE = `Para seleccionar una opción, simplemente escribe el número correspondiente:\n\n1️⃣ Información sobre Tecnoparque \n2️⃣ Registro y seguimiento de proyectos \n3️⃣ Asesorías y citas con expertos ‍\n4️⃣ Eventos y convocatorias \n5️⃣ Recursos y documentación \n6️⃣ Contacta un Asesor 🆘`;
+
+export const HORARIOS_MESSAGE = ` 🕘 *Horarios de Atención:*\nLunes a Viernes: 8:00 AM - 5:00 PM\n¡Te esperamos para impulsar juntos la innovación tecnológica! 🚀`;
+
+export const LINEAS_TECNOLOGICAS_MESSAGE = ` *Líneas Tecnológicas en Tecnoparque Bucaramanga* \nEscribe el número para consultar.\n\n1. Ingeniería y Diseño ️\n2. Biotecnología y Nanotecnología \n3. Electrónica y Telecomunicaciones ⚡\n4. Tecnologías Virtuales `;
+
+export const REQUISITOS_MESSAGE = `*Cómo acceder a Tecnoparque Nodo Bucaramanga* \n\n✅ *Requisitos para acceder:*\n\n1️⃣ *Tener una idea de proyecto de base tecnológica.*\n- Tecnoparque está dirigido a emprendedores, estudiantes, investigadores y empresas que desean desarrollar soluciones innovadoras en áreas como desarrollo de software, automatización, biotecnología, diseño industrial y más.\n- No es necesario que tengas conocimientos avanzados, pero sí una idea clara del problema que deseas resolver y la motivación para desarrollarla.\n\n2️⃣ *Registrarte en la plataforma oficial de Tecnoparque.*\n- Ingresa a https://redtecnoparque.com/ y crea una cuenta.\n- Completa tu perfil con información sobre tu proyecto y el área de interés.\n- Una vez registrado, selecciona el Nodo *Bucaramanga* y describe brevemente tu idea.\n\n3️⃣ *Asistir a una charla informativa y agendar una reunión con nuestros expertos.*\n- Luego de registrarte, recibirás una invitación para participar en una sesión informativa donde te explicaremos cómo funciona Tecnoparque y los beneficios que te ofrecemos.\n- Posteriormente, podrás agendar una reunión personalizada con un experto en tu área de interés, quien te orientará en el desarrollo de tu proyecto.\n\n`;
+
+export const REGISTRAR_PROYECTO_MESSAGE = `Para registrar un nuevo proyecto en Tecnoparque Nodo Bucaramanga, sigue estos pasos:\n\n1️⃣ Regístrate en la plataforma de Tecnoparque: (https://redtecnoparque.com/).\n2️⃣ Inicia sesión y crea una nueva idea de proyecto.\n3️⃣ Elige el nodo 'Bucaramanga' y describe tu idea.\n\n Tutorial: https://www.youtube.com/watch?v=jY7SiSPnlKc.`;
+
+export const CONSULTAR_PROYECTO_MESSAGE = `Para consultar el estado de tu proyecto en Tecnoparque Nodo Bucaramanga:\n\n Ingresa a la plataforma de Tecnoparque: [redtecnoparque.com](https://redtecnoparque.com/).\n Inicia sesión con tu usuario registrado.\n Dirígete a la sección 'Mis Proyectos' para ver el estado actual.\n\n Si tienes dudas, comunícate con el equipo de Tecnoparque Bucaramanga.`;
+
+export const REGISTRAR_EVENTO_MESSAGE = ` Puedes registrarte en los eventos y convocatorias a través de nuestro formulario oficial: https://redtecnoparque.com/eventos`;
+
+export const AGENDAR_CITA_MESSAGE = ` Para agendar una reunión con un asesor de Tecnoparque, accede a: https://redtecnoparque.com/citas`;
+
+export const PREGUNTAS_EVENTOS_MESSAGE = `❓ *Preguntas Frecuentes sobre Eventos y Convocatorias en Tecnoparque*\n\n1️⃣ *¿Quiénes pueden participar en los eventos?*\n  - Cualquier persona interesada en la tecnología y la innovación.\n\n2️⃣ *¿Los eventos tienen costo?*\n  - No, todos los eventos son gratuitos.\n\n3️⃣ *¿Cómo me inscribo en un evento?*\n  - Puedes registrarte en: https://redtecnoparque.com/eventos`;
+
+export const EVENTOS_MESSAGE = `*Eventos y Convocatorias en Tecnoparque Bucaramanga*\n\nMantente informado sobre los próximos eventos, talleres y convocatorias disponibles en Tecnoparque Nodo Bucaramanga.\n\n*Eventos disponibles:*\n- *Bootcamp de Innovación Tecnológica* - Fecha:  [Fecha del evento]\n- *Workshop de Inteligencia Artificial* - Fecha:  [Fecha del evento]\n\n*Convocatorias abiertas:*\n- *Convocatoria de Emprendimientos Tecnológicos* - Cierre:  [Fecha de cierre]\n- *Acceso a laboratorios y prototipado* - Cierre:  [Fecha de cierre]\n\nMás información y registro aquí: [Página oficial de eventos](https://redtecnoparque.com/eventos)`;
+
+// Mensajes de Líneas Tecnológicas
+export const ING_DISENO_MESSAGE = "*INGENIERÍA Y DISEÑO*\n\n" +
+    "Esta línea abarca el desarrollo de productos, desde la concepción hasta la fabricación de prototipos funcionales. Se enfoca en la optimización de diseños, simulaciones avanzadas y validaciones estructurales para crear soluciones innovadoras en herramientas, maquinaria y productos de consumo. 🏭\n\n" +
+    "*Diseño y simulación*:\n" +
+    " - Modelado de prototipos en 3D 🎨\n" +
+    " - Análisis, simulación y validación estructural 🔬\n" +
+    " - Animación y renderización fotorrealista 📽️\n\n" +
+    "*Prototipado*:\n" +
+    " - Prototipado manual y CAD/CAM 2D y 3D 🛠️\n\n" +
+    "*Ingeniería Inversa*:\n" +
+    " - Validación e inspección de productos 📏\n" +
+    " - Modelado y reconstrucción de piezas 🔄\n\n" +
+    "*Branding & Marketing Digital*:\n" +
+    " - Creación de marca y producto 📢";
+
+export const BIOTECNOLOGIA_MESSAGE = "*BIOTECNOLOGÍA Y NANOTECNOLOGÍA*\n\n"+
+    "Esta línea utiliza sistemas biológicos y nanotecnología para desarrollar nuevos productos en sectores como agroindustria, salud, medio ambiente e industria. 🌱🔬\n\n" +
+    "*Biotecnología Agroindustrial*:\n" +
+    " - Aprovechamiento de residuos agroindustriales 🍃\n" +
+    " - Tecnología en procesos de alimentos 🍞\n" +
+    " - Producción de alimentos para animales 🐄\n\n" +
+    "*Biotecnología Industrial*:\n" +
+    " - Desarrollo de biofármacos y antibióticos 💊\n" +
+    " - Producción de biocombustibles 🔥\n" +
+    " - Diseño de plantas biotecnológicas 🏗️\n\n" +
+    "*Biotecnología Vegetal*:\n" +
+    " - Cultivo de tejidos y propagación vegetal 🌾\n" +
+    " - Desarrollo de bioinsumos 🌍\n\n" +
+    "*Biotecnología Ambiental*:\n" +
+    " - Biorremediación y análisis de agua\n" +
+    " - Mejoramiento de suelos 🌱\n\n" +
+    "*Nanotecnología*:\n" +
+    " - Síntesis de nanopartículas y nuevos materiales\n" +
+    " - Aplicaciones en textiles, encapsulación y semiconductores";
+
+export const ELECTRONICA_MESSAGE =  "*ELECTRÓNICA Y TELECOMUNICACIONES*\n\n"+
+"Esta línea se enfoca en el desarrollo de soluciones para automatización, control, telecomunicaciones e internet de las cosas (IoT), creando sistemas inteligentes y eficientes. 🤖📡\n\n" +
+"*Automatización e Instrumentación*:\n" +
+" - Diseño Electrónico y Robótica 🔧\n" +
+" - Internet de las cosas (IoT) \n" +
+" - Análisis de señales y protocolos 📡\n" +
+" - Sistemas embebidos \n\n" +
+"*Infraestructura y Redes*:\n" +
+" - Redes Inteligentes 🔌\n" +
+" - Telecomunicaciones \n" +
+" - Redes y antenas 📡";
+
+export const TECNOLOGIAS_MESSAGE = "*TECNOLOGÍAS VIRTUALES*\n\n" +
+    "Esta línea explora el desarrollo de software, videojuegos, animación digital, inteligencia artificial, realidad virtual y aumentada para crear soluciones tecnológicas innovadoras. 🌍\n\n" +
+    "*Desarrollo de Software*:\n" +
+    " - Plataformas Web, Móviles y Standalone\n" +
+    " - Desarrollo de aplicaciones Cliente/Servidor\n\n" +
+    "*Videojuegos & Simulación*:\n" +
+    " - Diseño y desarrollo de videojuegos \n" +
+    " - Captura de movimiento \n\n" +
+    "*Producción Digital 2D & 3D*:\n" +
+    " - Animación Digital y Realidad Virtual \n" +
+    " - Inteligencia Artificial y computación avanzada 🤖";
+
+
+// Mensajes de Error
+export const OPCION_INVALIDA_MESSAGE = "Lo siento, no entendí tu selección. Por favor, elige una opción válida.";
+
+export const ERROR_MESSAGE = "Hubo un error al procesar tu solicitud. Por favor, inténtalo de nuevo.";
+
